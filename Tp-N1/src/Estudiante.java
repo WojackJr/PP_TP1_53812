90f23cbb71a46986fbb246822791a3179dbbb932
WojackJr <150086465+WojackJr@users.noreply.github.com>
2026-08-25 13:05:50 -0300

    jh

---
+++ Tp-N1/src/Estudiante.java
@@ -0,0 +1,13 @@
+public class Estudiante {
+    private String legajo;
+    private String nombre;
+    //getters and setters
+
+    public String getLegajo() {
+        return legajo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
---
+++ Tp-N1/src/Sala.java
@@ -0,0 +1,8 @@
+public class Sala {
+    private int id;
+    private String nombre;
+    //getter and setter
+    public String getNombre(){
+        return nombre;
+    }
+}
